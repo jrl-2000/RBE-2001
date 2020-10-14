c841720a12c9b2d2ef17298b0db51b754028073d
BBoxell <35980906+BBoxell@users.noreply.github.com>
2020-10-13 21:02:55 -0400

    Odometry Working!

--- src/Chassis.cpp
+++ src/Chassis.cpp
@@ -1,7 +1,11 @@
 
 #include <Chassis.h>
-const float pi = 3.1415926535897932384626433;
 
+void Chassis::initialize() {
+  drivePowerPID.pidInit(70,0,0);
+  turnPowerPID.pidInit(140,0,0);
+  turnPID.pidInit(1.8,0,0);
+}
 
 void Chassis::turnAngle(float degrees)
 {
@@ -21,7 +25,7 @@ while (encoders.getCountsRight() < getEncoderAngle || encoders.getCountsLeft() >
 void Chassis::driveDistance(float inches)
 {
   //Encoder clicks per certain distance
-  float encoderClicks = (CPR * inches) / (wheelDiameter * pi);
+  float encoderClicks = (CPR * inches) / (wheelDiameter * M_PI);
   encoders.getCountsAndResetRight();
   encoders.getCountsAndResetLeft();
 
@@ -36,4 +40,174 @@ void Chassis::driveDistance(float inches)
 void Chassis::drive(float effort)
 {
   motors.setEfforts(effort, effort);
+}
+
+float Chassis::getLeftEncoder() {
+  return encoders.getCountsLeft()/CPR;
+}
+
+float Chassis::getRightEncoder() {
+  return encoders.getCountsRight()/CPR;
+}
+
+void Chassis::updatePosition() {
+  curLeft = encoders.getCountsLeft()/CPR;
+  curRight = encoders.getCountsRight()/CPR; //step 1
+
+  deltaLeft = (curLeft - lastLeftPos)*(2*M_PI)*(wheelDiameter/2);
+  deltaRight = (curRight - lastRightPos)*(2*M_PI)*(wheelDiameter/2); //step 2
+
+  lastLeftPos = curLeft;
+  lastRightPos = curRight; //step 3
+
+  deltaLr = (curLeft - leftAtReset)*(2*M_PI)*(wheelDiameter/2); //step 4
+  deltaRr = (curRight - rightAtReset)*(2*M_PI)*(wheelDiameter/2);
+
+  thetaNew = (thetaReset + (deltaLr - deltaRr)/(Sl + Sr)); //step 5
+
+  deltaTheta = thetaNew - angle; //step 6
+
+  if (deltaTheta == 0) {
+    deltaY = deltaRight;
+  }
+
+  else {
+    deltaX = 0; //step 8
+    deltaY = (2*sin(deltaTheta/2))*(deltaRight/deltaTheta +Sr);
+  }
+
+  thetaM = angle + deltaTheta/2; //step 9
+
+  theta = atan2f(deltaY, deltaX);
+  radius = sqrt(deltaX*deltaX + deltaY*deltaY);
+  theta = theta-thetaM;                          //step 10
+  deltaX = radius*cos(theta);
+  deltaY = radius*sin(theta);
+
+  angle = thetaNew;
+  x = x + deltaX; //step 11
+  y = y + deltaY;
+}
+
+float Chassis::getX() {
+  return x;
+}
+
+float Chassis::getY() {
+  return y;
+}
+
+float Chassis::getAngleDegrees() {
+  return getAngle()*180/M_PI;
+}
+float Chassis::getAngle() {
+  return (modulus(angle+M_PI, 2*M_PI))-M_PI;
+  //return angle;
+}
+float Chassis::modulus(float a, float b) {
+  while(a<0) {
+    a+=b;
+  }
+  while(a>b) {
+    a-=b;
+  }
+  return a;
+}
+bool Chassis::turnToAngle(float targetAngle) {
+  bool atPoint = false;
+
+  float referenceAngle = 0;
+  if (targetAngle-getAngleDegrees()>180) {
+    referenceAngle = getAngleDegrees()+360;
+  }
+  else if (targetAngle-getAngleDegrees()<-180) {
+    referenceAngle = getAngleDegrees()-360;
+  }
+  else {
+    referenceAngle = getAngleDegrees();
+  }
+  float turnPower = turnPID.pidCalculate(targetAngle, referenceAngle);
+  
+  motors.setEfforts(turnPower,-turnPower);
+
+  if (abs(getAngleDegrees()-targetAngle)<1.5) {
+    atPoint = true;
+  }
+  return atPoint;
+}
+
+bool Chassis::moveToPoint(float targetX, float targetY) {
+	bool atPoint = false;
+	float targetAngle =0;
+	float power =0;
+	float turnPower =0;
+  if (millis()-lastSlewTime>10){
+    lastSlewTime = millis()-5;
+  }
+  power = -drivePowerPID.pidCalculate(0, sqrt(pow(targetY-getY(),2) + pow(targetX-getX(),2)));
+  power = slewRateCalculate(power);
+  //Serial.println(power);
+ // if (targetY-getY())
+  targetAngle = -1*(atan2f((targetY-getY()),(targetX-getX()))-M_PI/2);
+
+  Serial.print(targetAngle*180/M_PI);
+
+  float projection = ((targetX-getX())*sin(getAngle())+(targetY-getY())*cos(getAngle()));
+
+  if (projection < 0) {
+    power = -power;
+    targetAngle = modulus((targetAngle+M_PI)+M_PI, 2*M_PI)-M_PI;
+  }
+
+  float referenceAngle = 0;
+  if (targetAngle-getAngle()>M_PI) {
+    referenceAngle = getAngle()+(2*M_PI);
+  }
+  else if (targetAngle-getAngle()<-M_PI) {
+    referenceAngle = getAngle()-(2*M_PI);
+  }
+  else {
+
+     referenceAngle = getAngle();
+   }
+  turnPower = turnPowerPID.pidCalculate(targetAngle, referenceAngle);
+  Serial.println("\nTarget Angle: ");
+
+  // Serial.println("\nAngle");
+  // Serial.print(getAngleDegrees());
+  // Serial.println("\nTurnPower");
+  // Serial.print(turnPower);
+
+ // power = 0;
+  motors.setEfforts((power + turnPower)*0.35, (power - turnPower)*0.35);
+
+  if (sqrt(pow(targetY-getY(),2) + pow(targetX-getX(),2)) < 0.5) {
+    atPoint = true;
+  }
+  return atPoint;
+}
+
+float Chassis::slewRateCalculate (float desiredRate) {
+		float deltaTime = millis()-lastSlewTime;
+		float desiredAccel = (desiredRate -lastSlewRate)/deltaTime;
+		float addedRate;
+		float newRate;
+
+		if (fabs(desiredAccel) < maxAccel || (desiredAccel<0 && desiredRate>0) || (desiredAccel>0 && desiredRate<0)) {
+		    addedRate = desiredAccel*deltaTime;
+		    newRate = addedRate+lastSlewRate;
+		}
+		else {
+		    addedRate = ((desiredAccel>0)? 1: -1)*maxAccel*deltaTime;
+        newRate = addedRate+lastSlewRate;
+		}
+	  lastSlewTime = lastSlewTime+deltaTime;
+	  lastSlewRate = newRate;
+
+		float returnVal = newRate;
+		return returnVal;
+}
+
+void Chassis::stopAllMotors(){
+  motors.setEfforts(0,0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,282 @@
 #include <Arduino.h>
-#include <Romi32U4.h>
+#include "Romi32U4.h"
 #include "Chassis.h"
-#include <Romi32U4Buttons.h>
+#include "Romi32U4Buttons.h"
 #include "RangeFinder.h"
 #include "BlueMotor.h"
+#include "IRdecoder.h"
+#include "RemoteConstants.h"
+#include "servo32u4.h"
+#include "PID.h"
+
 
 Rangefinder rangeFinder;
 BlueMotor arm;
-Chassis baseBot;
+Chassis chassis;
+IRDecoder decoder;
+Servo32U4 servo;
 Romi32U4ButtonA pb;
-void setup() {
-  rangeFinder.setup();
-  arm.setup();
-  Serial.begin(9600);
-}
-long positionOld = 0;
-int currentEffort = 0;
-unsigned long startTime = millis();
-bool active = false;
-void loop() {
 
+bool paused = false;
+
+//State machine states
+enum States
+{
+  CLOSE_JAW_START,
+  LIFT_PLATE_45,
+  BACK_UP_45,
+  TURN_TO_PLATFORM_45,
+  GO_TO_PLATFORM_45,
+  LOWER_ARM_45,
+  OPEN_JAW_45_PLATFORM,
+  CLOSE_JAW_45_PLATFORM,
+  RAISE_ARM_45_PLATFORM,
+  MOVE_AWAY_FROM_PLATFORM_45,
+  TURN_TO_45_ROOF,
+  DRIVE_TO_ROOF_AND_RAISE_PLATE_45,
+  DROP_PLATE_45,
+  OPEN_JAW_45_END,
+  BACK_UP_FROM_45,
+  TURN_RIGHT_M,
+  DRIVE_TO_CLEAR_ROOF_M,
+  TURN_LEFT_M,
+  DRIVE_TO_PLATFORM_LINE_M,
+  TURN_LEFT_M2,
+  DRIVE_TO_ROOF_LINE_M,
+  TURN_TO_25_ROOF_1,
+  DRIVE_TO_25_ROOF_AND_RAISE_ARM,
+  CLOSE_JAW_25_START,
+  LIFT_PLATE_25,
+  BACK_UP_25,
+  TURN_TO_PLATFORM_25,
+  GO_TO_PLATFORM_25,
+  LOWER_ARM_25,
+  OPEN_JAW_25_PLATFORM,
+  CLOSE_JAW_25_PLATFORM,
+  RAISE_ARM_25_PLATFORM,
+  MOVE_AWAY_FROM_PLATFORM_25,
+  TURN_TO_25_ROOF_2,
+  DRIVE_TO_ROOF_AND_RAISE_PLATE_25,
+  DROP_PLATE_25,
+  OPEN_JAW_FINAL,
+  BACK_UP_FINAL,
+  STOPPED
+} state;
+
+
+void checkRemote()
+{
+  if (decoder.getKeyCode() == remotePlayPause)
+  {
+    paused = !paused;
+    Serial.println(paused ? "Paused" : "Running");
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  arm.setup();
+  rangeFinder.setup();
+  decoder.init();
+  arm.reset();
+  state = CLOSE_JAW_START;
+  chassis.initialize();
+}
+
+void doStateMachine()
+{
+  switch (state)
+  {
+  case CLOSE_JAW_START:
+    
+    state =  LIFT_PLATE_45;
+    break;
+  case LIFT_PLATE_45:
+
+    state = BACK_UP_45;
+    break;
+  case BACK_UP_45:
+    if (chassis.moveToPoint(-11,0)) {
+      state = TURN_TO_PLATFORM_45;
+      chassis.stopAllMotors();
+    }
+    break;
+  case TURN_TO_PLATFORM_45:
+    if (chassis.turnToAngle(90)) {
+      state = GO_TO_PLATFORM_45;
+      chassis.stopAllMotors();
+    }
+    break;
+  case GO_TO_PLATFORM_45:
+
+    state = LOWER_ARM_45;
+    break;
+  case LOWER_ARM_45:
+
+    state = OPEN_JAW_45_PLATFORM;
+    break;
+  case OPEN_JAW_45_PLATFORM:
+
+    state = CLOSE_JAW_45_PLATFORM;
+    break;
+  case CLOSE_JAW_45_PLATFORM:
+
+    state = RAISE_ARM_45_PLATFORM;
+    break;
+  case RAISE_ARM_45_PLATFORM:
+
+    state = MOVE_AWAY_FROM_PLATFORM_45;
+    break;
+  case MOVE_AWAY_FROM_PLATFORM_45:
+
+    state = TURN_TO_45_ROOF;
+    break;
+  case TURN_TO_45_ROOF:
+
+    state = DRIVE_TO_ROOF_AND_RAISE_PLATE_45;
+    break;
+  case DRIVE_TO_ROOF_AND_RAISE_PLATE_45:
+
+    state = DROP_PLATE_45;
+    break;
+  case DROP_PLATE_45:
+
+    state = OPEN_JAW_45_END;
+    break;
+  case OPEN_JAW_45_END:
+
+    state = BACK_UP_FROM_45;
+    break;
+  case BACK_UP_FROM_45:
+
+    state = TURN_RIGHT_M;
+    break;
+  case TURN_RIGHT_M:
+
+    state = DRIVE_TO_CLEAR_ROOF_M;
+    break;
+  case  DRIVE_TO_CLEAR_ROOF_M:
+
+    state = TURN_LEFT_M;
+    break;
+  case TURN_LEFT_M:
+
+    state = DRIVE_TO_PLATFORM_LINE_M;
+    break;
+  case DRIVE_TO_PLATFORM_LINE_M:
+
+    state = TURN_LEFT_M2;
+    break;
+  case TURN_LEFT_M2:
+
+    state = DRIVE_TO_ROOF_LINE_M;
+    break;
+  case DRIVE_TO_ROOF_LINE_M:
+
+    state = TURN_TO_25_ROOF_1;
+    break;
+  case TURN_TO_25_ROOF_1:
+
+    state = DRIVE_TO_25_ROOF_AND_RAISE_ARM;
+    break;
+  case DRIVE_TO_25_ROOF_AND_RAISE_ARM:
+
+    state = CLOSE_JAW_25_START;
+    break;
+  case CLOSE_JAW_25_START:
+
+    state = LIFT_PLATE_25;
+    break;
+  case LIFT_PLATE_25:
+
+    state = BACK_UP_25;
+    break;
+  case BACK_UP_25:
+
+    state = TURN_TO_PLATFORM_25;
+    break;
+  case TURN_TO_PLATFORM_25:
+
+    state =  GO_TO_PLATFORM_25;
+    break;
+  case GO_TO_PLATFORM_25:
+
+    state = LOWER_ARM_25;
+    break;
+  case LOWER_ARM_25:
+
+    state = OPEN_JAW_25_PLATFORM;
+    break;
+  case OPEN_JAW_25_PLATFORM:
+
+    state = CLOSE_JAW_25_PLATFORM;
+    break;
+  case CLOSE_JAW_25_PLATFORM:
+
+    state = RAISE_ARM_25_PLATFORM;
+    break;
+  case RAISE_ARM_25_PLATFORM:
+
+    state = MOVE_AWAY_FROM_PLATFORM_25;
+    break;
+  case MOVE_AWAY_FROM_PLATFORM_25:
+
+    state = TURN_TO_25_ROOF_2;
+    break;
+  case TURN_TO_25_ROOF_2:
+
+    state = DRIVE_TO_ROOF_AND_RAISE_PLATE_25;
+    break;
+  case DRIVE_TO_ROOF_AND_RAISE_PLATE_25:
+
+    state = DROP_PLATE_25;
+    break;
+  case DROP_PLATE_25:
+
+    state = OPEN_JAW_FINAL;
+    break;
+  case OPEN_JAW_FINAL:
+
+    state = BACK_UP_FINAL;
+    break;
+  case BACK_UP_FINAL:
+
+    state = STOPPED;
+    break;
+  case STOPPED:
+    
+    break;
+
+  }
+
+
+}
+
+bool buttonPressed = false;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  rangeFinder.loop();
+  //doStateMachine();
+  Serial.println("\n\nX:\tY:\tAngle:\n");
+  Serial.print(chassis.getX());
+  Serial.print("\t");
+  Serial.print(chassis.getY());
+  Serial.print("\t");
+  Serial.print(chassis.getAngleDegrees());
+
+  if (pb.isPressed()) {
+    buttonPressed = true;
+  }
+  if (buttonPressed) {
+   // doStateMachine();
+   if (chassis.moveToPoint(-10,-10)) {
+      //state = TURN_TO_PLATFORM_45;
+      chassis.stopAllMotors();
+      buttonPressed = false;
+    }
+  }
+  chassis.updatePosition();
+  delay(5);
 }
